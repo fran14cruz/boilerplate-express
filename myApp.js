df78c1863de3a72fa0c21e52008dd64f62ffe1c1
Francisco Cruz <fran14.ich@gmail.com>
2023-03-01 16:35:08 -0500

    Comments

--- myApp.js
+++ myApp.js
@@ -2,13 +2,16 @@ let express = require('express');
 let app = express();
 console.log('Hello World')
 
+// now you can do: mysite.com/public/style.css
 app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
   let absolutePath = __dirname + '/views/index.html';
+  // also: res.send();
   res.sendFile(absolutePath);
 });
 
+// sending a json object
 app.get('/json', function(req, res) {
   const data = {
     "message": "Hello json"
